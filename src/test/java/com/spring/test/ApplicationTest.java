d1624af851f01c05e791c6e260d450bb14e0a422
weimeilayer <weimeilayer@163.com>
2016-12-30 20:06:44 +0800

    master

--- src/test/java/com/spring/test/ApplicationTest.java
+++ src/test/java/com/spring/test/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.spring.test;
+/*package com.spring.test;
 
 import javax.annotation.Resource;
 
@@ -21,3 +21,4 @@ public class ApplicationTest {
 		Service.getFindUserInfoAll();
 	}
 }
+*/
