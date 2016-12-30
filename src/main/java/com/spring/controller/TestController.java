b444df8c796b1b0d19e33de3d870165e74106767
weimeilayer <weimeilayer@163.com>
2016-12-30 15:24:27 +0800

    master

--- src/main/java/com/spring/controller/TestController.java
+++ src/main/java/com/spring/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	@RequestMapping("/")
+	public String toString(){
+		return "Hello world！";
+	}
 	/*@Autowired
 	private TestInfo test;
 	
